34fcfce2796bc6dbe6d32decde10b3260b79ac58
Hamid Sukardi <hamid@Hamids-MBP.bbrouter>
2024-06-01 12:48:20 +0700

    create controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Todo\TodoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,6 +11,9 @@ Route::get('/halo', function () {
     echo "halo";
 });
 
-Route::get('/todo', function () {
-    return view('todo.app');
-});
+// Route::get('/todo', function () {
+//     return view('todo.app');
+// });
+
+Route::get('/todo', [TodoController::class, 'index']);
+Route::post('/todo', [TodoController::class, 'store']);
